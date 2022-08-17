1751168a8259abb0885df9d5f1f71bb9ac374cf8
Felipe Iotti <felipeizago@hotmail.com>
2022-08-17 12:08:58 -0300

    finalizado2

--- src/modules/transactions/useCases/Transactions/deleteTransactions/DeleteTransactionsUseCase.ts
+++ src/modules/transactions/useCases/Transactions/deleteTransactions/DeleteTransactionsUseCase.ts
@@ -1,3 +1,4 @@
+import { IPersonsRepository } from "@modules/transactions/repositories/IPersonsRepository";
 import { ITransactionsRepository } from "@modules/transactions/repositories/ITransactionsRepository";
 import { inject, injectable } from "tsyringe";
 
@@ -10,12 +11,38 @@ export class DeleteTransactionsUseCase {
   constructor(
       @inject("TransactionsRepository")
       private transactionsRepository: ITransactionsRepository,
+      @inject("PersonsRepository")
+      private personsRepository: IPersonsRepository,
     ){}
 
   async execute({id}:IRequest): Promise<void> {
     
+    const transaction= await this.transactionsRepository.findById(id);
+
+    const memberPay = await this.personsRepository.findByName(transaction.memberPay);
+
+    if(memberPay){
+      memberPay.amount= transaction.amount;
+      await this.personsRepository.update(memberPay);
+    }
+    
+
+    const members = await this.personsRepository.list();
+    
+    members.map(async(member) => {
+      const membersToDivide =  transaction.persons.find((item)=> item.name === member.name) 
+
+      
+      //console.log(membersToDivide )
+      if(membersToDivide){
+        
+        member.amount = - membersToDivide.amount;
+        
+        await this.personsRepository.update(member);
+        }
+    });
+
+
     await this.transactionsRepository.delete(id);
-    
-    
   }
 }
